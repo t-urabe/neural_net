6fa7338d595fdbfafa3a28d2cf1b3482e016a853
t-urabe <t-urabe@wave.plala.or.jp>
2016-09-27 10:13:11 +0900

    Initial Commit

---
+++ neural_net/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  neural_net
+//
+//  Created by TU on 2016/09/27.
+//  Copyright © 2016年 TU. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
